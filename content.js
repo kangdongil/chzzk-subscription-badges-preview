c4c2755615c609da2468a2228ff80bdfbad9bf99
kangdongil <akang8150@naver.com>
2026-01-24 00:56:49 +0900

    feat: 정기구독관리 팝업 감지

--- content.js
+++ content.js
@@ -1,6 +1,9 @@
 (() => {
   console.log("content script loaded");
 
+  // ENVIRONMENT & STATE
+  let isPopup = false;
+
   const getPageContext = () => {
     const path = location.pathname;
 
@@ -11,6 +14,7 @@
     return null;
   };
 
+  // SELECTORS
   const CONTROL_ROOT_SELECTOR = {
     channel: '[class*="channel_profile_control"]',
     live: '[class*="video_information_control"]',
@@ -26,7 +30,62 @@
         ),
       ].find((el) => el.textContent.trim() === "구독중");
     },
+    get subscribeContainer() {
+      return [
+        ...document.querySelectorAll('[class*="subscribe_container"]'),
+      ].find((el) => {
+        const header = el.querySelector('[class*="popup_header"]');
+        return header?.textContent.trim() === "내 정기구독 관리";
+      });
+    },
   };
 
   window.SELECTORS = SELECTORS; // DEBUG
+
+  // OBSERVER
+  function waitForElement(getter, root = document.body) {
+    return new Promise((resolve) => {
+      const el = getter();
+      if (el) {
+        resolve(el);
+        return;
+      }
+
+      const observer = new MutationObserver(() => {
+        const el = getter();
+        if (!el) return;
+
+        observer.disconnect();
+        resolve(el);
+      });
+
+      observer.observe(root, { childList: true, subtree: true });
+    });
+  }
+
+  // CORE ACTION
+  function modifyContainer(container) {
+    console.log(container);
+  }
+
+  // LIFECYCLE
+  async function getSubscribeContainer() {
+    if (isPopup) return;
+    isPopup = true;
+
+    const container = await waitForElement(() => SELECTORS.subscribeContainer);
+    const closeBtn = container.querySelector('[class*="popup_action"]');
+
+    modifyContainer(container);
+
+    closeBtn.addEventListener("click", () => {
+      isPopup = false;
+    });
+  }
+
+  async function init() {
+    const btn = await waitForElement(() => SELECTORS.subscribedBtn);
+    btn.addEventListener("click", getSubscribeContainer);
+  }
+  init();
 })();
